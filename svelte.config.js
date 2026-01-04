8fa6379ab85f330ff9defe5520ac3d4c43d0b27c
tch-hub <tch27work@gmail.com>
2026-01-04 20:25:07 +0900

    feat: configure base path for SvelteKit application

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,12 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: '/image-compressor'
+		}
+	}
 };
 
 export default config;
